848ad2598930a3e5da5837c9e084ee0723dea0c2
uni <unibtc@gmail.com>
2018-08-22 18:33:07 +0800

    additional details

--- run.sh
+++ run.sh
@@ -2,53 +2,52 @@ echo Staring Perfomance Test
 rm -rf results.txt
 echo "-----"
 echo Express
-echo "\nExpress\n" >> results.txt
+echo -e "\nExpress" >> results.txt
 node expressserver.js &
 sleep 2
-ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep 'Time taken for tests:\|Requests per second:\|Time per request:' >> results.txt
+ab -k -n 50000 -c 100 http://127.0.0.1:8000/ | grep 'Time taken for tests:\|Requests per second:\|Time per request:' >> results.txt
 echo Closing expressserver
-kill -KILL $(pidof node)
-
+kill -0 $! && kill $!
+sleep 2
 echo ""
 echo HapiJs
-echo "\nHapiJs\n" >> results.txt ;
+echo -e "\nHapiJs" >> results.txt ;
 node hapiserver.js &
 sleep 2
-ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep 'Time taken for tests:\|Requests per second:\|Time per request:' >> results.txt
+ab -k -n 50000 -c 100 http://127.0.0.1:8000/ | grep 'Time taken for tests:\|Requests per second:\|Time per request:' >> results.txt
 echo Closing hapiserver ;
-kill -KILL $(pidof node)
-
+kill -0 $! && kill $!
+sleep 2
 echo ""
 echo Raw NodeJs
-echo "\nRaw NodeJs\n" >> results.txt ;
+echo -e "\nRaw NodeJs" >> results.txt ;
 node rawnodeserver.js &
 sleep 2
-ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep 'Time taken for tests:\|Requests per second:\|Time per request:' >> results.txt
+ab -k -n 50000 -c 100 http://127.0.0.1:8000/ | grep 'Time taken for tests:\|Requests per second:\|Time per request:' >> results.txt
 echo Closing rawnodeserver ;
-kill -KILL $(pidof node)
-
+kill -0 $! && kill $!
+sleep 2
 echo ""
 echo Restify
-echo "\nRestify\n" >> results.txt ;
+echo -e "\nRestify" >> results.txt ;
 node restifyserver.js &
 sleep 2
-ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep 'Time taken for tests:\|Requests per second:\|Time per request:' >> results.txt
+ab -k -n 50000 -c 100 http://127.0.0.1:8000/ | grep 'Time taken for tests:\|Requests per second:\|Time per request:' >> results.txt
 echo Closing restifyserver ;
-kill -KILL $(pidof node)
-
+kill -0 $! && kill $!
+sleep 2
 echo ""
 echo Koa2
-echo "\nKoa2\n" >> results.txt ;
+echo -e "\nKoa2" >> results.txt ;
 node koa2server.js &
 sleep 2
-ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep 'Time taken for tests:\|Requests per second:\|Time per request:' >> results.txt
+ab -k -n 50000 -c 100 http://127.0.0.1:8000/ | grep 'Time taken for tests:\|Requests per second:\|Time per request:' >> results.txt
 echo Closing koa2server ;
-kill -KILL $(pidof node)
+kill -0 $! && kill $!
+echo ""
 sleep 2
-echo ""
-
 echo Performance Test complete!
-echo ""
+echo "-------------RESULT-------------"
 cat results.txt
 exit
 
