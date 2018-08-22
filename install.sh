7f40406e4d10f445864e8e7b898e68188b28ebbe
uni <unibtc@gmail.com>
2018-08-22 16:40:56 +0800

    update install script

--- install.sh
+++ install.sh
@@ -1,19 +1,9 @@
 apt-get install apache2-utils
-apt-get install npm
-npm install n -g
-n latest
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+source ~/.bashrc
+nvm install 10
+nvm use 10
 npm install hapi
 npm install express
 npm install restify
 npm install koa
-npm install koa2
-npm install n
-npm install total.js
-npm install adonis-cli -g
-adonis new yardstick
-cp yardstick-changes/app/Http/kernel.js yardstick/app/Http/kernel.js
-cp yardstick-changes/app/Http/routes.js yardstick/app/Http/routes.js
-npm install sails -g
-sails new test-project
-cp test-project-changes/api/controllers/ApiController.js test-project/api/controllers/ApiController.js
-cp test-project-changes/config/routes.js test-project/config/routes.js
