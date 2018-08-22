54cee78ba6baf6bbb3eb556af03c532253a3abe1
uni <unibtc@gmail.com>
2018-08-22 16:51:44 +0800

    export nvm

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 10
 nvm use 10
 npm install hapi
