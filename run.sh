d1dc8f41945713950df1e74fbe2ab8736ecf3e86
uni <unibtc@gmail.com>
2018-08-22 17:26:31 +0800

    clean results.txt and echo it once tests are done

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 echo Staring Perfomance Test
+rm -rf results.txt
 echo Express
-echo express >> results.txt ;
+echo Express >> results.txt ;
 node expressserver.js & sleep 5 ; 
 ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
 echo closing expressserver ;
@@ -8,7 +9,7 @@ pkill -f expressserver > /dev/null 2>&1;
 sleep 5 
 echo ""
 echo HapiJs
-echo hapi >> results.txt ;
+echo HapiJs >> results.txt ;
 node hapiserver.js & sleep 5 ;
 ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
 echo closing hapiserver ;
@@ -16,24 +17,28 @@ pkill -f hapiserver > /dev/null 2>&1;
 sleep 5 
 echo ""
 echo Raw NodeJs
-echo node >> results.txt ;
+echo Raw NodeJs >> results.txt ;
 node rawnodeserver.js & sleep 5 ; 
 ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
 pkill -f rawnodeserver > /dev/null 2>&1;
 sleep 5 
 echo ""
 echo Restify
-echo restify >> results.txt ;
+echo Restify >> results.txt ;
 node restifyserver.js & sleep 5 ; 
 ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
 pkill -f restifyserver > /dev/null 2>&1;
 sleep 5 
 echo ""
 echo Koa2
-echo koa2 >> results.txt ;
+echo Koa2 >> results.txt ;
 node koa2server.js & sleep 5 ; 
 ab -k -n 50000 -c 100 -t 20 http://127.0.0.1:8000/ | grep "Requests per second:" >> results.txt ;
 pkill -f koa2server > /dev/null 2>&1;
 sleep 5 
+echo ""
 
+echo Performance Test complete!
+echo ""
+echo $(cat results.txt)
 
